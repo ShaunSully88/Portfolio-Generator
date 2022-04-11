2ebaad43931bc9c45248806062dcf5e7dd917504
ShaunSully88 <98717384+ShaunSully88@users.noreply.github.com>
2022-04-11 12:03:18 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs)
 
 const printProfileData = profileDataArr => {
 for (let i = 0; i < profileDataArr.length; i++) {

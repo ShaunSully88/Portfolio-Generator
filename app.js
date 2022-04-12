aa863188528c85bfedc16becf35e5d21210577bd
ShaunSully88 <98717384+ShaunSully88@users.noreply.github.com>
2022-04-12 10:21:30 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
 console.log(profileDataArgs)
@@ -23,7 +25,7 @@ return `
 `;
 };
 fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+  if (err) throw new Error(err);
 
 console.log('Portfolio complete! Check out index.html to see the output!');
 });

040cbc5283122694f1dfe4b20a939cd2d4ee23bb
ShaunSully88 <98717384+ShaunSully88@users.noreply.github.com>
2022-04-12 09:28:04 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //const profileDataArgs = process.argv.slice(2, process.argv.length);
 //console.log(profileDataArgs)
 
-const generatePage = (userName, githubName) => `Name: ${Jane}, Github: ${janehub}`;
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
 console.log(generatePage());
 

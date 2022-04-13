a62059be97d12025144a3f7d4a03413227759175
Shaun Sullivan <shaunsully88@gmail.com>
2022-04-13 10:25:32 -0400

    inquirer package installed

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
+const inquirer = require('inquirer')
 const fs = require('fs');
 const generatePage = require('./src/page-template');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-const [name, github] = profileDataArgs;
+inquirer
+  .prompt ([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+const pageHTML = generatePage(name, github);
 
-console.log(profileDataArgs)
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
+fs.writeFile('index.html', pageHTML, err => {
+  if (err) throw err;
 
 console.log('Portfolio complete! Check out index.html to see the output!');
 });

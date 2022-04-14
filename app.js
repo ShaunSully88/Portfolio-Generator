3e69c6a5ab9c2dbe4a6f45bfc4793396308b4f2f
Shaun Sullivan <shaunsully88@gmail.com>
2022-04-14 16:20:31 -0400

    added filter method to help seperate featured projects

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
-//const fs = require('fs');
-//const generatePage = require('./src/page-template');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
 
 const promptUser = () => {
@@ -136,15 +136,14 @@ const promptProject = portfolioData => {
 
 promptUser()
 .then(promptProject)
-.then(portfolioData => {console.log(portfolioData);
-});
+.then(portfolioData => {
 
-//const pageHTML = generatePage(name, github);
+  const pageHTML = generatePage(portfolioData);
 
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//   if (err) throw err;
-
-// console.log('Portfolio complete! Check out index.html to see the output!');
+ fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw new Error(err);
+ });
+// console.log('Page created! Check out index.html in this directory to see it!');
 //});
-
+});
